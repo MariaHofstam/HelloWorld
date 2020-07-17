16ce661a0076916ffd8e90fecd4b3efce1b1d956
Maria Hofstam <45755355+MariaHofstam@users.noreply.github.com>
2020-07-17 12:12:14 +0200

    Update from GitHub
    GitHub says hello Maria!

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Maria says hello GitHub");
+        
+        //Editing from GitHub
+        System.out.println("GitHub says hello Maria!");
     }
 }
